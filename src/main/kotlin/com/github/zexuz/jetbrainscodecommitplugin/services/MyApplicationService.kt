b63f1b3cdd78ea17c56fd14e9e856b33672f8460
GitHub Action <action@github.com>
2021-10-12 08:12:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zexuz/jetbrainscodecommitplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zexuz.jetbrainscodecommitplugin.services
+
+import com.github.zexuz.jetbrainscodecommitplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
