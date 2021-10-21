5878611fac4c3d2c1923036b5712b8ae3f843ee4
Robin Edbom <robin.edbom@consultant.volvo.com>
2021-10-21 10:10:20 +0200

    dump

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,10 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("com.amazonaws:aws-java-sdk:1.12.86")
+}
+
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
     pluginName.set(properties("pluginName"))
--- src/main/kotlin/com/github/zexuz/jetbrainscodecommitplugin/toolWindow/MyToolWindow.kt
+++ src/main/kotlin/com/github/zexuz/jetbrainscodecommitplugin/toolWindow/MyToolWindow.kt
@@ -1,21 +1,35 @@
 package com.github.zexuz.jetbrainscodecommitplugin.toolWindow
 
+import com.amazonaws.services.codecommit.AWSCodeCommitClientBuilder
+import com.amazonaws.services.codecommit.model.ListRepositoriesRequest
+import com.intellij.diff.DiffContentFactory
+import com.intellij.diff.DiffContext
+import com.intellij.diff.DiffManager
+import com.intellij.diff.contents.DocumentContent
+import com.intellij.diff.requests.DiffRequest
+import com.intellij.diff.requests.SimpleDiffRequest
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.vcs.actions.VcsContextFactory
 import com.intellij.openapi.wm.ToolWindow
+import com.intellij.tasks.context.BranchContextTracker
+import java.awt.event.ActionEvent
+import java.util.*
+import javax.swing.ImageIcon
 import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.ImageIcon
-import java.awt.event.ActionListener
-import java.awt.event.ActionEvent
-import java.util.*
 
 class MyToolWindow(toolWindow: ToolWindow) {
     private var refreshToolWindowButton: JButton? = null
     private var hideToolWindowButton: JButton? = null
+    private var diffToolWindowButton: JButton? = null
     private var currentDate: JLabel? = null
     private var currentTime: JLabel? = null
     private var timeZone: JLabel? = null
     var content: JPanel? = null
+
+    private val log: Logger = Logger.getInstance(MyToolWindow::class.java)
 
     fun currentDateTime() {
         // Get current date and time
@@ -34,11 +48,42 @@ class MyToolWindow(toolWindow: ToolWindow) {
         str_gmt_Offset = if (gmt_Offset > 0) "GMT + $str_gmt_Offset" else "GMT - $str_gmt_Offset"
         timeZone!!.text = str_gmt_Offset
         timeZone!!.icon = ImageIcon(javaClass.getResource("/toolWindow/Time-zone-icon.png"))
+
+        log.info("This is a debug message")
+
+        val client = AWSCodeCommitClientBuilder.defaultClient()
+        val repos = client.listRepositories(ListRepositoriesRequest());
+        log.info("Lenght is ${repos.repositories.size}")
+        repos.repositories.forEach {
+            log.info(it.repositoryName)
+        }
+
     }
 
     init {
         hideToolWindowButton!!.addActionListener { e: ActionEvent? -> toolWindow.hide(null) }
         refreshToolWindowButton!!.addActionListener { e: ActionEvent? -> currentDateTime() }
+        diffToolWindowButton!!.addActionListener { e: ActionEvent? -> showDiffWindow() }
         currentDateTime()
     }
+
+    private fun showDiffWindow() {
+        com.intellij.openapi.vcs.diff.ItemLatestState().
+        val project = ProjectManager.getInstance().openProjects.first()
+        val content1: DocumentContent = DiffContentFactory.getInstance().create("text1")
+        val content2: DocumentContent = DiffContentFactory.getInstance().create("text2")
+        val request = SimpleDiffRequest("Window Title", content1, content2, "Title 1", "Title 2")
+        DiffManager.getInstance().showDiff(project, request)
+    }
+}
+
+class Test : com.intellij.diff.DiffTool{
+    override fun getName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun canShow(context: DiffContext, request: DiffRequest): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
